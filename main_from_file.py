874632c8f5f52955e41ab7804dada2d0f0e5aef9
ana-kop <ana.kop20@gmail.com>
2020-11-07 11:36:56 +0000

    07/11: polygon class

--- main_from_file.py
+++ main_from_file.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
-
 import matplotlib
 import matplotlib.pyplot as plt
+import sys
 
 matplotlib.use('TkAgg')
 
@@ -12,7 +12,7 @@ class Plotter:
         plt.figure()
 
     def add_polygon(self, xs, ys):
-        plt.fill(xs, ys, 'lightgray', label='Polygon')
+        plt.plot(xs, ys, 'lightgray', label='Polygon')
 
     def add_point(self, x, y, kind=None):
         if kind == "outside":
@@ -46,3 +46,97 @@ class Point:
         self.x = x
         self.y = y
 
+
+class Polygon:
+    def __init__(self, points):
+        #points: a list of Points in clockwise order.
+        self.points = points
+
+    @property
+    def edges(self):
+        #Returns a list of tuples that each contain 2 points of an edge
+        edge_list = []
+        for i, p in enumerate(self.points):
+            p1 = p
+            p2 = self.points[(i + 1) % len(self.points)]
+            edge_list.append((p1, p2))
+
+        return edge_list
+
+    def mbr_check_if_inside(self, point):
+        res = False
+        if (min_x <= point.x <= max_x) and (min_y <= point.y <= max_y):
+            res = True
+        return res
+
+    def contains(self, point):
+        # _huge is used to act as infinity if we divide by 0
+        _huge = sys.float_info.max
+        # _eps is used to make sure points are not on the same line as vertexes
+        _eps = 0.00001
+
+        # We start on the outside of the polygon
+        inside = False
+        for edge in self.edges:
+            # Make sure A is the lower point of the edge
+            a, b = edge[0], edge[1]
+            if a.y > b.y:
+                a, b = b, a
+
+            # Make sure point is not at same height as vertex
+            if point.y == a.y or point.y == b.y:
+                point.y += _eps
+
+            if point.y > b.y or point.y < a.y or point.x > max(a.x, b.x):
+                # The horizontal ray does not intersect with the edge
+                continue
+
+            if point.x < min(a.x, b.x):  # The ray intersects with the edge
+                inside = not inside
+                continue
+
+            try:
+                m_edge = (b.y - a.y) / (b.x - a.x)
+            except ZeroDivisionError:
+                m_edge = _huge
+
+            try:
+                m_point = (point.y - a.y) / (point.x - a.x)
+            except ZeroDivisionError:
+                m_point = _huge
+
+            if m_point >= m_edge:
+                # The ray intersects with the edge
+                inside = not inside
+                continue
+
+        return inside
+
+# define a boundary function
+
+
+# Read a list of x, y coordinates from a comma-separated values (CSV) file
+
+
+# Find coordinates for MBR
+
+
+# make a list of 4 points so that we can make a polygon
+
+
+# make a list of coordinates for testing from csv
+
+
+# find which points are outside MBR
+
+
+# classify points
+
+
+# combine results into one sorted list to be written into the file
+
+
+# write into file
+
+
+# plot
