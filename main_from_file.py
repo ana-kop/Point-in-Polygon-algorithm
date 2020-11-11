d53eb7ee03550cb0ba2925ba4f52b160e5cde656
ana-kop <ana.kop20@gmail.com>
2020-11-11 20:25:12 +0000

    11/11: points dictionary + read file function

--- main_from_file.py
+++ main_from_file.py
@@ -12,7 +12,10 @@ class Plotter:
         plt.figure()
 
     def add_polygon(self, xs, ys):
-        plt.plot(xs, ys, 'lightgray', label='Polygon')
+        plt.plot(xs, ys, 'black', label='Polygon', lw=0.7)
+
+    def add_mbr(self, xs, ys):
+        plt.plot(xs, ys, 'darkgray', label='MBR', linestyle='--')
 
     def add_point(self, x, y, kind=None):
         if kind == "outside":
@@ -23,15 +26,6 @@ class Plotter:
             plt.plot(x, y, "go", label='Inside')
         else:
             plt.plot(x, y, "ko", label='Unclassified')
-
-    def green_point(self, x, y):
-        plt.plot(x, y, "go")
-
-    def red_point(self, x, y):
-        plt.plot(x, y, "ro")
-
-    def blue_point(self, x, y):
-        plt.plot(x, y, "bo")
 
     def show(self):
         handles, labels = plt.gca().get_legend_handles_labels()
@@ -49,12 +43,12 @@ class Point:
 
 class Polygon:
     def __init__(self, points):
-#points: a list of Points in clockwise order.
+        # points: a list of Points in clockwise order.
         self.points = points
 
     @property
     def edges(self):
-#Returns a list of tuples that each contain 2 points of an edge
+        # Returns a list of tuples that each contain 2 points of an edge
         edge_list = []
         for i, p in enumerate(self.points):
             p1 = p
@@ -115,7 +109,7 @@ class Polygon:
 
         return inside
 
-# define a boundary function
+    # define a boundary function
     def boundary(self, point):
         _eps = 0.00001
         location = 'outside'
@@ -125,24 +119,29 @@ class Polygon:
             if a.y > b.y:
                 a, b = b, a
 
-            distance_a_b = ((a.x - b.x) ** 2 + (a.y - b.y) ** 2)**(1/2)
-            distance_a_point = ((a.x - point.x) ** 2 + (a.y - point.y) ** 2)**(1/2)
-            distance_b_point = ((point.x - b.x) ** 2 + (point.y - b.y) ** 2)**(1/2)
+            distance_a_b = ((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** (1 / 2)
+            distance_a_point = ((a.x - point.x) ** 2 + (a.y - point.y) ** 2) ** (1 / 2)
+            distance_b_point = ((point.x - b.x) ** 2 + (point.y - b.y) ** 2) ** (1 / 2)
 
             if distance_a_point + distance_b_point == distance_a_b:
                 location = 'boundary'
         return location
 
 
+def read_points_from_file(path, mode, list_for_points):
+    with open(path, mode) as f:
+        for line in (f.readlines()[1:]):
+            items = line.split(',')
+            name = str(items[0])
+            x = float(items[1])
+            y = float(items[2])
+            list_for_points.append(Point(name, x, y))
+    return list_for_points
+
+
 # Read a list of x, y coordinates from a comma-separated values (CSV) file
 polygon_points = []
-with open('polygon.csv', 'r') as f:
-    for line in (f.readlines()[1:]):
-        items = line.split(',')
-        name = str(items[0])
-        x = float(items[1])
-        y = float(items[2])
-        polygon_points.append(Point(name, x, y))
+read_points_from_file('polygon.csv', 'r', polygon_points)
 p = Polygon(polygon_points)
 
 xs = []
@@ -165,74 +164,47 @@ mbr_y = [min_y, max_y, max_y, min_y, min_y]
 
 # make a list of coordinates for testing from csv
 input_points = []
-with open('input.csv', 'r') as d:
-    for line in d.readlines()[1:]:
-        items = line.split(',')
-        name = str(items[0])
-        x = float(items[1])
-        y = float(items[2])
-        input_points.append(Point(name, x, y))
+read_points_from_file('input.csv', 'r', input_points)
+
+points_dictionary = {}
+for point in input_points:
+    points_dictionary.update({point.name: [point.x, point.y, 'unclassified']})
 
 # find which points are outside MBR
 inside_mbr = []
-outside_mbr = []
 for point in input_points:
     if p.mbr_check_if_inside(point) is True:
         inside_mbr.append(point)
     else:
-        outside_mbr.append(point)
+        points_dictionary[point.name][2] = 'outside'
+print(points_dictionary)
 
 # classify points
-on_boundary = []
 to_be_classified = []
 for point in inside_mbr:
     if p.boundary(point) == 'boundary':
-        on_boundary.append(point)
+        points_dictionary[point.name][2] = 'boundary'
     else:
         to_be_classified.append(point)
 
-inside_polygon = []
-outside_polygon = []
 for point in to_be_classified:
     if p.contains(point) is True:
-        inside_polygon.append(point)
+        points_dictionary[point.name][2] = 'inside'
     else:
-        outside_polygon.append(point)
-
-# combine results into one sorted list to be written into the file
-results = []
-for point in outside_mbr:
-    r = [int(point.name), ',outside']
-    results.append(r)
-for point in outside_polygon:
-    r = [int(point.name), ',outside']
-    results.append(r)
-for point in on_boundary:
-    r = [int(point.name), ',boundary']
-    results.append(r)
-for point in inside_polygon:
-    r = [int(point.name), ',inside']
-    results.append(r)
-results.sort(key=lambda x: x[0])
+        points_dictionary[point.name][2] = 'outside'
 
 # write into file
 output_file = open('output.csv', 'w+')
 output_file.write('id,category')
-for point in results:
+for key, values in points_dictionary.items():
     output_file.write('\n')
-    output_file.write(str(point[0]) + point[1])  # is it okay for id to be string in the output?
+    output_file.write(key + ',' + values[2])  # is it okay for id to be string in the output?
 output_file.close()
 
 # plot
 plotter = Plotter()
 plotter.add_polygon(xs, ys)
-plotter.add_polygon(mbr_x, mbr_y)
-for point in outside_mbr:
-    plotter.red_point(point.x, point.y)
-for point in outside_polygon:
-    plotter.red_point(point.x, point.y)
-for point in inside_polygon:
-    plotter.green_point(point.x, point.y)
-for point in on_boundary:
-    plotter.blue_point(point.x, point.y)
+plotter.add_mbr(mbr_x, mbr_y)
+for key, values in points_dictionary.items():
+    plotter.add_point(values[0], values[1], values[2])
 plotter.show()
